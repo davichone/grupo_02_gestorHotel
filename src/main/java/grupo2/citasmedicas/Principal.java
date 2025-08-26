64212b206d33ecf6068cab8ff5baf54066766f18
David Rolando <d4sh18@gmail.com>
2025-08-25 19:39:29 -0500

    primer commit

---
+++ src/main/java/grupo2/citasmedicas/Principal.java
@@ -0,0 +1,11 @@
+
+package grupo2.citasmedicas;
+
+//@author David Rolando
+ 
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
